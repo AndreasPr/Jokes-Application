c0c6c498b5fa16242ef172cf756c1f844325c097
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-04-19 18:54:42 -0400

    Add the files to Github repository

---
+++ src/main/java/springframework/joke/jokeapplication/JokeapplicationApplication.java
@@ -0,0 +1,13 @@
+package springframework.joke.jokeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springframework/joke/jokeapplication/JokeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.joke.jokeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
